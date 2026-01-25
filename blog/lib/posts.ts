b30c13ed8aa52a10711f7028c51af3a9677c2d60
Brian Ellis <brianellis1997@gmail.com>
2026-01-24 22:08:19 -0600

    Fix post sorting - newest first

--- blog/lib/posts.ts
+++ blog/lib/posts.ts
@@ -37,7 +37,7 @@ export function getAllPosts(): PostMeta[] {
       };
     });
 
-  return posts.sort((a, b) => (a.date < b.date ? 1 : -1));
+  return posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 }
 
 export function getPostBySlug(slug: string): Post | null {
